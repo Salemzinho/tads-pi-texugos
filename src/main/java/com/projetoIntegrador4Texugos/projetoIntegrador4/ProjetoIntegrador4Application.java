1e027af7665dc2d8886e6a287015f17f476970fe
Salemzinho <luan_co30@hotmail.com>
2022-03-07 11:26:30 -0300

    Escopo projeto

---
+++ src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/ProjetoIntegrador4Application.java
@@ -0,0 +1,13 @@
+package com.projetoIntegrador4Texugos.projetoIntegrador4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegrador4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegrador4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoIntegrador4Texugos/projetoIntegrador4/ProjetoIntegrador4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoIntegrador4Texugos.projetoIntegrador4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegrador4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
