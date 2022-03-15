2fbd56d74f055f65884bad8e82ac62e5a3c63fb3
unknown <gessione>
2022-03-15 12:13:10 -0300

    Att Service e controller

--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/UsuarioController.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/UsuarioController.java
@@ -1,12 +1,19 @@
 package com.projetoIntegrador4Texugos.projetoIntegrador4.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.projetoIntegrador4Texugos.projetoIntegrador4.dto.UsuarioInput;
@@ -47,5 +54,35 @@ public class UsuarioController {
 		return "painel";
 	}
 	
+	@GetMapping("")
+	public ResponseEntity<List<Usuario>> findAll() {
+		return ResponseEntity.ok().body( this.usuService.findAll());
+	}
 	
+	@GetMapping("/{idUsuario}")
+	public ResponseEntity<Usuario> findOne(@PathVariable int idUsuario) {
+		return ResponseEntity.ok().body( this.usuService.findOne(idUsuario));
+	}
+
+	@PostMapping
+	public ResponseEntity<Usuario> save(@Valid @RequestBody Usuario novoUsuario) {
+		return ResponseEntity.ok().body(this.usuService.salvar(novoUsuario));
+	}
+
+	@PatchMapping("/{idUsuario}")
+	public ResponseEntity<Usuario> update(@Valid @PathVariable int idUsuario,
+			@RequestBody Usuario updateUsuario) {
+		return ResponseEntity.ok().body(this.usuService.update(idUsuario, updateUsuario));
+	}
+
+	@DeleteMapping("/{idUsuario}")
+	public void delete(@PathVariable int idUsuario) {
+		this.usuService.delete(idUsuario);
+	}
+
+	@DeleteMapping("many/{idUsuario}")
+	public void deleteMany(@PathVariable int[] id) {
+
+		this.usuService.deleteMany(id);
+	}
 }
--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/service/UsuarioService.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/service/UsuarioService.java
@@ -1,11 +1,17 @@
 package com.projetoIntegrador4Texugos.projetoIntegrador4.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projetoIntegrador4Texugos.projetoIntegrador4.exception.RegraNegocioException;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Usuario;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.repository.UsuarioRepository;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+
 
 @Service
 public class UsuarioService {
@@ -22,6 +28,50 @@ public class UsuarioService {
 		}
 			
 		return usuarioRepo.save(usuario);
+	}
+	
+	public List<Usuario> findAll() {
+		List<Usuario> list = this.usuarioRepo.findAll();
+		return list.stream().collect(Collectors.toCollection(ArrayList::new));
+	}
+	
+	public Usuario findOne(int idUsuario) {
+		return this.usuarioRepo.findById(idUsuario)
+				.orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
+	}	
+		
+	
+	public void delete(int id) {
+		this.usuarioRepo.deleteById(id);
+	}
+
+	// deletar VARAIOS
+	public void deleteMany(int[] ids) {
+		for (int i : ids) {
+			this.usuarioRepo.deleteById(i);
+		}		
+	}
+	
+	
+	public Usuario update(int idUsuario, Usuario updateUsuario) {
+		Optional<Usuario> usuarioOptional = this.usuarioRepo.findById(idUsuario);
+		if (usuarioOptional.isPresent()) {
+			Usuario usuarioExistente = usuarioOptional.get();
+			usuarioExistente.setId(updateUsuario.getId());
+			usuarioExistente.setNome(updateUsuario.getNome());
+			usuarioExistente.setCPF(updateUsuario.getCPF());
+			usuarioExistente.setDataNascimento(updateUsuario.getDataNascimento());
+			usuarioExistente.setEmail(updateUsuario.getEmail());
+			usuarioExistente.setIsAtivo(updateUsuario.getIsAtivo());
+			usuarioExistente.setSenha(updateUsuario.getSenha());
+			usuarioExistente.setTelefone(updateUsuario.getTelefone());
+			usuarioExistente.setTipo(updateUsuario.getTipo());
+					
+
+			return this.usuarioRepo.save(usuarioExistente);
+		} else {
+			throw new RuntimeException("Usuario(a) nao encontrado(a)");
+		}
 	}
 	
 }
