a018380ac182d9c75c5ae9e4b37b9858a706e8df
PEDRO HENRIQUE SOUZA PECANHA <pedro.hspecanha@senacsp.edu.br>
2022-03-15 19:31:01 -0300

    Addicionando o metodo putmapping inativa usuario

--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/UsuarioController.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/UsuarioController.java
@@ -1,22 +1,20 @@
 package com.projetoIntegrador4Texugos.projetoIntegrador4.controller;
 
-import java.util.List;
+import java.security.Principal;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.projetoIntegrador4Texugos.projetoIntegrador4.dto.UsuarioInput;
+import com.projetoIntegrador4Texugos.projetoIntegrador4.model.TipoUsuario;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Usuario;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.service.UsuarioService;
 
@@ -44,6 +42,25 @@ public class UsuarioController {
 		usuService.salvar(usuario);
 		
 		return "redirect:/usuario";
+		
+	}
+	
+	@PutMapping("/{id}")
+	public String inativarUsuario(@PathVariable("id") int id, Principal principal) {
+		
+		Usuario usuarioLogado = usuService.findByEmail(principal.getName());
+		
+		if(usuarioLogado.getTipo().compareTo(TipoUsuario.ADMINISTRADOR)==0) {
+			usuService.findOne(id);
+			
+			
+			
+			return "redirect:/usuario";
+		}
+		
+		else {
+			return "redirect:/usuario?erro=1";
+		}
 		
 	}
 	
