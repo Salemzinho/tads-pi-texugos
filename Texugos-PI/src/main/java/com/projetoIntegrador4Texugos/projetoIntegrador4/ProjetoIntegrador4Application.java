e0f03283dc62d278339a9383620602d844d20dc4
RODOLPHO RAMOS DE ALCANTARA <rodolpho.ralcantara@senacsp.edu.br>
2022-03-22 19:13:30 -0300

    Adição da tela de edição e endpoints para consulta de um unico usuario e edição do mesmo

--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/ProjetoIntegrador4Application.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/ProjetoIntegrador4Application.java
@@ -1,7 +1,14 @@
 package com.projetoIntegrador4Texugos.projetoIntegrador4;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.projetoIntegrador4Texugos.projetoIntegrador4.model.TipoUsuario;
+import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Usuario;
+import com.projetoIntegrador4Texugos.projetoIntegrador4.repository.UsuarioRepository;
 
 @SpringBootApplication
 public class ProjetoIntegrador4Application {
@@ -9,5 +16,14 @@ public class ProjetoIntegrador4Application {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoIntegrador4Application.class, args);
 	}
+	
+	@Bean
+    public CommandLineRunner demoData(UsuarioRepository usu) {
+        return args -> { 
+
+        	Usuario usuario = new Usuario(1, "Administrador", "administrador@texugos.com", 11111111111L, new BCryptPasswordEncoder().encode("admin1234"),"999.999.999-99", "2022-03-01", true, TipoUsuario.ADMINISTRADOR);
+        	usu.save(usuario);
+        };
+    }
 
 }
