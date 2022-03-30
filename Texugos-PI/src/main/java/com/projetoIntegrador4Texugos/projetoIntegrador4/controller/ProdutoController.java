1e1890665fe99e103751036b5f9a2cd190ba3f08
PEDRO HENRIQUE SOUZA PECANHA <pedro.hspecanha@senacsp.edu.br>
2022-03-30 19:32:14 -0300

    Adicionando método de editar produto e arrumando imports

--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/ProdutoController.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/ProdutoController.java
@@ -5,9 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.projetoIntegrador4Texugos.projetoIntegrador4.dto.UsuarioInput;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.ImagemModel;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Produto;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.TipoUsuario;
@@ -149,6 +155,19 @@ public class ProdutoController {
 	    model.addAttribute("produtos", produtos);
 		
 		return "produto/produto-list";
+	}
+	
+	@PostMapping("/{id}/editarProduto")
+	public String editarProduto(@PathVariable int id, Principal principal, Produto produto) {
+		Usuario usuarioLogado = usuService.findByEmail(principal.getName());
+		if(usuarioLogado.getTipo().compareTo(TipoUsuario.ESTOQUISTA)==0) {
+			produto.setIdProd(id);
+			prodService.update(id, produto);
+			return "redirect:/produto/";
+		}
+		else {
+			return "redirect:/usuario?erro=unauthorized";
+		}
 	}
 	
 	@PostMapping("/{id}/deletar")
