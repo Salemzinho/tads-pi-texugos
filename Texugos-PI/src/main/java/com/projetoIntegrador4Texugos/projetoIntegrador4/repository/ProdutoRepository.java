4a09d4101ec89117f13aa4dbd312eba4a6c0171d
PEDRO HENRIQUE SOUZA PECANHA <pedro.hspecanha@senacsp.edu.br>
2022-04-13 20:53:09 -0300

    fix

--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/repository/ProdutoRepository.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/repository/ProdutoRepository.java
@@ -1,9 +1,6 @@
 package com.projetoIntegrador4Texugos.projetoIntegrador4.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Produto;
