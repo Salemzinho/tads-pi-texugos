9a39f96ea8c02f0db1992c77489eecee9c5f4e8f
LUAN COSTA DE OLIVEIRA <luan.coliveira8@senacsp.edu.br>
2022-04-26 20:08:30 -0300

    Foreach no perfil

--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/PerfilUsuarioController.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/PerfilUsuarioController.java
@@ -1,19 +1,30 @@
 package com.projetoIntegrador4Texugos.projetoIntegrador4.controller;
 
-import com.projetoIntegrador4Texugos.projetoIntegrador4.model.ClienteModel;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.EnderecoModel;
+import com.projetoIntegrador4Texugos.projetoIntegrador4.service.EnderecoService;
 
 @Controller
 public class PerfilUsuarioController {
 	
+	@Autowired
+	private EnderecoService enderecoService;
+	
+	
 	@GetMapping("/perfil")
-	public String perfilUsuario() {
+	public String perfilUsuario(Model model) {
+		List<EnderecoModel> enderecos = enderecoService.findAll();
+		model.addAttribute("enderecos", enderecos);
+		
 		return "perfil";
     }
 	
