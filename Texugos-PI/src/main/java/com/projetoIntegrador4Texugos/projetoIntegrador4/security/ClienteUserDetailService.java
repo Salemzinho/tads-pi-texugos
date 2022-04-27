6d1cda5ea09b1b7db46b9f5e440e0acf905428df
RODOLPHO RAMOS DE ALCANTARA <rodolpho.ralcantara@senacsp.edu.br>
2022-04-27 20:01:17 -0300

    fix

---
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/security/ClienteUserDetailService.java
@@ -0,0 +1,41 @@
+package com.projetoIntegrador4Texugos.projetoIntegrador4.security;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.projetoIntegrador4Texugos.projetoIntegrador4.model.ClienteModel;
+import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Usuario;
+import com.projetoIntegrador4Texugos.projetoIntegrador4.repository.ClienteRepository;
+import com.projetoIntegrador4Texugos.projetoIntegrador4.repository.UsuarioRepository;
+
+
+@Component
+public class ClienteUserDetailService implements UserDetailsService {
+
+	@Autowired
+	ClienteRepository cliRepo;
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		ClienteModel cliente = cliRepo.findByEmail(email); 
+		
+		if (cliente == null) {
+            throw new UsernameNotFoundException("Usuário não encontrado!");
+        }
+
+        return cliente;
+	}
+	
+	public Collection<? extends GrantedAuthority> authorities(ClienteModel cliente) {
+        return cliente.getAuthorities(); 
+    }
+
+}
