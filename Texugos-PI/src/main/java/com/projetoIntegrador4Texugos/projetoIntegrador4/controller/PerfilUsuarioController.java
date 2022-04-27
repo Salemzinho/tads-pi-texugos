f6ae66435830917cbe1966e4df9168043ec198c2
salemzinho <luan_co30@hotmail.com>
2022-04-27 16:49:20 -0300

    arrumando rota perfil

--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/PerfilUsuarioController.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/PerfilUsuarioController.java
@@ -33,11 +33,6 @@ public class PerfilUsuarioController {
 		return "/perfil";
 	}
 
-	@GetMapping("/")
-	public String perfilUsuario() {
-		return "perfil";
-	}
-
 	@PostMapping("/inserirEndereco")
 	public String insereEndereco(ClienteModel cliente, Principal principal) {
 		ClienteModel clienteLogado = clienteService.findByEmail(principal.getName());
