23320fd8ed4234c052e570dbdef763099d15f09a
Pedrinhsp <pedrinho.top07@gmail.com>
2022-04-26 23:43:27 -0300

    adicionando controller de alterar endereço

--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/PerfilUsuarioController.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/PerfilUsuarioController.java
@@ -1,66 +1,55 @@
 package com.projetoIntegrador4Texugos.projetoIntegrador4.controller;
 
-<<<<<<< HEAD
-
+import java.security.Principal;
 import java.util.List;
 
-=======
-import java.security.Principal;
-
->>>>>>> 08c4d557e6865c930319596ad34aec813c682e6a
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
-
-
-
-import com.projetoIntegrador4Texugos.projetoIntegrador4.model.EnderecoModel;
-import com.projetoIntegrador4Texugos.projetoIntegrador4.service.EnderecoService;
-=======
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.ClienteModel;
+import com.projetoIntegrador4Texugos.projetoIntegrador4.model.EnderecoModel;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.TipoUsuario;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.service.ClienteService;
->>>>>>> 08c4d557e6865c930319596ad34aec813c682e6a
+import com.projetoIntegrador4Texugos.projetoIntegrador4.service.EnderecoService;
 
 @Controller
 @RequestMapping("/perfil")
 public class PerfilUsuarioController {
-	
-	@Autowired
 
+	@Autowired
 	private EnderecoService enderecoService;
-	
-	
-	@GetMapping("/perfil")
+
+	@Autowired
+	private ClienteService clienteService;
+
+	@GetMapping("/")
 	public String perfilUsuario(Model model) {
 		List<EnderecoModel> enderecos = enderecoService.findAll();
 		model.addAttribute("enderecos", enderecos);
-		
-	private ClienteService clienteService;
-	
+		return "/perfil";
+	}
+
 	@GetMapping("/")
 	public String perfilUsuario() {
 		return "perfil";
-    }
-	
+	}
+
 	@PostMapping("/inserirEndereco")
-	public String insereEndereco (ClienteModel cliente, Principal principal) {
+	public String insereEndereco(ClienteModel cliente, Principal principal) {
 		ClienteModel clienteLogado = clienteService.findByEmail(principal.getName());
 		if (clienteLogado.getTipo().compareTo(TipoUsuario.CLIENTE) == 0) {
-//			Produto prod = prodService.save(produto);
-//			imgService.armazenar(produto.getImagens());
-			
-			//Terminar a logica de inserção do endereço
-			
-			return "redirect:/admin/produto/";
+			for (int i = 0; i < cliente.getEndereco().size(); i++) {
+				if (cliente.getEndereco().get(i).getIsPadrao()) {
+					enderecoService.updateAddress(cliente.getId(), cliente.getEndereco().get(i));
+				}
+			}
+			return "/perfil";
 		}
 		return "perfil";
 	}
-	
 
 }
--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/service/EnderecoService.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/service/EnderecoService.java
@@ -1,13 +1,14 @@
 package com.projetoIntegrador4Texugos.projetoIntegrador4.service;
 
 
-import com.projetoIntegrador4Texugos.projetoIntegrador4.repository.EnderecoRepository;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.projetoIntegrador4Texugos.projetoIntegrador4.model.EnderecoModel;
 
-import java.util.List;
+import com.projetoIntegrador4Texugos.projetoIntegrador4.model.EnderecoModel;
+import com.projetoIntegrador4Texugos.projetoIntegrador4.repository.EnderecoRepository;
 
 
 @Service
@@ -16,10 +17,28 @@ public class EnderecoService {
     @Autowired
 	private EnderecoRepository enderecoRepo;
 
-
     public List<EnderecoModel> findAll() {
 		List<EnderecoModel> list = this.enderecoRepo.findAll();
 		return list;
+	}
+    
+    public EnderecoModel updateAddress(int id, EnderecoModel enderecoModel) {						
+		Optional<EnderecoModel> op = this.enderecoRepo.findById(id);
+		if(op.isPresent()){
+			EnderecoModel obj = op.get();
+			obj.setBairro(enderecoModel.getBairro());
+			obj.setCEP(enderecoModel.getCEP());
+			obj.setComplemento(enderecoModel.getComplemento());
+			obj.setLocalidade(enderecoModel.getLocalidade());
+			obj.setIsFaturamento(enderecoModel.getIsFaturamento());
+			obj.setIsPadrao(enderecoModel.getIsPadrao());
+			obj.setNumero(enderecoModel.getNumero());
+			obj.setUF(enderecoModel.getUF());
+			obj.setLogradouro(enderecoModel.getLogradouro());
+			return this.enderecoRepo.save(obj);
+		} else {
+			throw new RuntimeException("Endereço não encontrado!");
+		}
 	}
 	
 	
