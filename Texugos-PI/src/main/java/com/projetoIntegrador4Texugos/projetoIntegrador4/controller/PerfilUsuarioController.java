ed52b034e35a6eb11dab6859debdfa08304b940f
LUAN COSTA DE OLIVEIRA <luan.coliveira8@senacsp.edu.br>
2022-04-27 19:05:01 -0300

    fix merge

--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/PerfilUsuarioController.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/PerfilUsuarioController.java
@@ -7,15 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.ClienteModel;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.EnderecoModel;
-import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Produto;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.TipoUsuario;
-import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Usuario;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.service.ClienteService;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.service.EnderecoService;
 
@@ -51,17 +48,5 @@ public class PerfilUsuarioController {
 		return "perfil";
 	}
 	
-	@PostMapping("/{id}/editarProduto")
-	public String editarProduto(@PathVariable int id, Principal principal, Produto produto) {
-		
-		if(usuarioLogado.getTipo().compareTo(TipoUsuario.ESTOQUISTA)==0) {
-			produto.setIdProd(id);
-			prodService.update(id, produto);
-			return "redirect:/admin/produto/";
-		}
-		else {
-			return "redirect:/usuario?erro=unauthorized";
-		}
-	}
 
 }
