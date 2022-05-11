87d6acf04e493f4e1e8c61392778b1ccf187edfd
salemzinho <luan_co30@hotmail.com>
2022-05-11 14:40:03 -0300

    calculando valor total carrinho

--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/CarrinhoController.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/CarrinhoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Compra;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.ItensCompraModel;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Produto;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.service.ProdutoService;
@@ -22,14 +23,45 @@ public class CarrinhoController {
 	private ProdutoService prodService;
 
 	private List<ItensCompraModel> itensCompra = new ArrayList<ItensCompraModel>();
+	private Compra compra = new Compra();
+
+	private void calcularTotal(){
+		compra.setValorTotal(0.);
+		for(ItensCompraModel it: itensCompra){
+			compra.setValorTotal(compra.getValorTotal() + it.getValorTotal());
+		}
+	}
 
 	@GetMapping("/carrinho")
 	public ModelAndView carrinho(Model model) {
 		ModelAndView mv = new ModelAndView("carrinho");
+
+		calcularTotal();
+		mv.addObject("compra", compra);
 		mv.addObject("listaItens", itensCompra); 
 		    
 		return mv;
 	}
+
+	
+	@GetMapping("/alterarQuantidade/{id}/{acao}") 
+	public ModelAndView alterarQuantidade (@PathVariable Long id, @PathVariable Integer acao){
+		ModelAndView mv = new ModelAndView("carrinho");
+		for (ItensCompraModel it : itensCompra) { 
+			if (it.getProduto().getIdProd().equals(id)) { 
+				if (acao.equals(1)) {
+					it.setQuantidade(it.getQuantidade () + 1); 
+				} 
+				else if (acao == 0) {
+					it.setQuantidade(it.getQuantidade() - 1);
+					break;
+				}
+			}
+			mv.addObject("listaItens", itensCompra);
+		}
+		return mv;
+	}
+	
 
 	@GetMapping("/adicionarCarrinho/{id}") 
 	public ModelAndView adicionarCarrinho(@PathVariable int id) throws Exception {
@@ -38,11 +70,22 @@ public class CarrinhoController {
 		Produto prod = prodService.findOne(id);
 		ItensCompraModel item = new ItensCompraModel(); 
 
-		item.setProduto(prod); 
-		item.setValorUnitario(prod.getPrecoUnitProd()); 
-		item.setQuantidade(item.getQuantidade() + 1); 
-		item.setValorTotal(item.getQuantidade() * item.getValorUnitario()); 
-		itensCompra.add(item);
+		int controle = 0;
+		for(ItensCompraModel it:itensCompra){
+			if(it.getProduto().getIdProd().equals(prod.getIdProd())){
+				it.setQuantidade(it.getQuantidade()+1);
+				controle = 1;
+				break;
+			}
+		}
+		if(controle == 0){
+			item.setProduto(prod); 
+			item.setValorUnitario(prod.getPrecoUnitProd()); 
+			item.setQuantidade(item.getQuantidade() + 1); 
+			item.setValorTotal(item.getValorTotal() + (item.getQuantidade() * item.getValorUnitario())); 
+			itensCompra.add(item);
+		}
+
 		mv.addObject("listaItens", itensCompra); 
 
 		return mv;
---
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/model/Compra.java
@@ -0,0 +1,49 @@
+package com.projetoIntegrador4Texugos.projetoIntegrador4.model;
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "compra")
+public class Compra implements Serializable {
+    public Compra(){
+        super();
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private ClienteModel ClienteModel;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCompra = new Date();
+    private String formaPagamento;
+    private Double valorTotal = 0.;
+
+    public Double getValorTotal(){
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal){
+        this.valorTotal = valorTotal;
+    }
+
+
+}
