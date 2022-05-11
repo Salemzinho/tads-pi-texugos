adc098074ede061ef7c571190ef14a1e84eeedd2
Pedrinhsp <pedrinho.top07@gmail.com>
2022-05-10 23:59:23 -0300

    adicionando controller de redirecionamento para checkout

--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/CarrinhoController.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/CarrinhoController.java
@@ -1,5 +1,6 @@
 package com.projetoIntegrador4Texugos.projetoIntegrador4.controller;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,24 +9,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.ItensCompraModel;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Produto;
+import com.projetoIntegrador4Texugos.projetoIntegrador4.model.TipoUsuario;
+import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Usuario;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.service.ProdutoService;
+import com.projetoIntegrador4Texugos.projetoIntegrador4.service.UsuarioService;
 
 
 @Controller
+@RequestMapping("/carrinho")
 public class CarrinhoController {
 
 	@Autowired
 	private ProdutoService prodService;
+	
+	@Autowired
+	private UsuarioService usuService;
 
 	private List<ItensCompraModel> itensCompra = new ArrayList<ItensCompraModel>();
 	
 	private ModelAndView mv = new ModelAndView("carrinho");
 	
-	@GetMapping("/carrinho")
+	@GetMapping("/")
 	public ModelAndView carrinho(Model model) {
 		
 		mv.addObject("listaItens", itensCompra); 
@@ -63,6 +72,17 @@ public class CarrinhoController {
 		}
 //		mv.addObject("listaItens", itensCompra); 
 		return mv;
+	}
+	
+	@GetMapping("/finalizarPedido")
+	public String finalizarPedido( Principal principal) {
+		Usuario usuarioLogado = usuService.findByEmail(principal.getName());
+		if(usuarioLogado.getTipo().compareTo(TipoUsuario.CLIENTE)==0) {
+			return "redirect:/pagamento/";
+		}
+		else {
+			return "redirect:/usuario?erro=unauthorized";
+		}
 	}
 	
 	
