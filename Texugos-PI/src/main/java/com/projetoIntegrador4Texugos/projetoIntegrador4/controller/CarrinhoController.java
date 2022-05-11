f689d5b1e903fccee8ccfa5d0db375d7bd1982fe
salemzinho <luan_co30@hotmail.com>
2022-05-11 00:22:31 -0300

    arrumando carrinho

--- Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/CarrinhoController.java
+++ Texugos-PI/src/main/java/com/projetoIntegrador4Texugos/projetoIntegrador4/controller/CarrinhoController.java
@@ -1,6 +1,5 @@
 package com.projetoIntegrador4Texugos.projetoIntegrador4.controller;
 
-import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,34 +8,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.ItensCompraModel;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Produto;
-import com.projetoIntegrador4Texugos.projetoIntegrador4.model.TipoUsuario;
-import com.projetoIntegrador4Texugos.projetoIntegrador4.model.Usuario;
 import com.projetoIntegrador4Texugos.projetoIntegrador4.service.ProdutoService;
-import com.projetoIntegrador4Texugos.projetoIntegrador4.service.UsuarioService;
 
 
 @Controller
-@RequestMapping("/carrinho")
 public class CarrinhoController {
 
 	@Autowired
 	private ProdutoService prodService;
-	
-	@Autowired
-	private UsuarioService usuService;
 
 	private List<ItensCompraModel> itensCompra = new ArrayList<ItensCompraModel>();
-	
-	private ModelAndView mv = new ModelAndView("carrinho");
-	
-	@GetMapping("/")
+
+	@GetMapping("/carrinho")
 	public ModelAndView carrinho(Model model) {
-		
+		ModelAndView mv = new ModelAndView("carrinho");
 		mv.addObject("listaItens", itensCompra); 
 		    
 		return mv;
@@ -44,6 +33,7 @@ public class CarrinhoController {
 
 	@GetMapping("/adicionarCarrinho/{id}") 
 	public ModelAndView adicionarCarrinho(@PathVariable int id) throws Exception {
+		ModelAndView mv = new ModelAndView("carrinho");
 
 		Produto prod = prodService.findOne(id);
 		ItensCompraModel item = new ItensCompraModel(); 
@@ -57,32 +47,4 @@ public class CarrinhoController {
 
 		return mv;
 	}
-	
-	@GetMapping("/removerCarrinho/{id}") 
-	public ModelAndView removerCarrinho(@PathVariable int id) throws Exception {
-
-		Produto prod = prodService.findOne(id);
-		ItensCompraModel item = new ItensCompraModel(); 
-
-		item.setProduto(prod); 
-		item.setQuantidade(item.getQuantidade() - 1); 
-		if(item.getQuantidade()==0) {
-			itensCompra.remove(item);
-		}
-//		mv.addObject("listaItens", itensCompra); 
-		return mv;
-	}
-	
-	@GetMapping("/finalizarPedido")
-	public String finalizarPedido( Principal principal) {
-		Usuario usuarioLogado = usuService.findByEmail(principal.getName());
-		if(usuarioLogado.getTipo().compareTo(TipoUsuario.CLIENTE)==0) {
-			return "redirect:/pagamento/";
-		}
-		else {
-			return "redirect:/usuario?erro=unauthorized";
-		}
-	}
-	
-	
 }
